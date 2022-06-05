2246b2e622f018e521e7366c110c8e0393ed5d2b
Yunjoo Son <68374234+yjooooo@users.noreply.github.com>
2022-06-05 11:22:09 +0900

    [Silver II] Title: 좌표 압축, Time: 3368 ms, Memory: 326052 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.kt
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.kt
@@ -10,3 +10,18 @@ fun main() {
     }
     println(answer)
 }
+
+/* 방법2
+import java.lang.StringBuilder
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val originalX = readLine()!!.split(" ").map { it.toInt() }
+    val sortedX = originalX.distinct().sorted()
+    val answer = StringBuilder()
+    originalX.forEach {
+        answer.append("${sortedX.binarySearch(it)} ")
+    }
+    println(answer)
+}
+*/
