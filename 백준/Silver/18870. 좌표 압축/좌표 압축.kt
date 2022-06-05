607148f6a14bedb079b40f11cbd7ef8674658ca2
Yunjoo Son <68374234+yjooooo@users.noreply.github.com>
2022-06-05 11:19:08 +0900

    [Silver II] Title: 좌표 압축, Time: 3368 ms, Memory: 326052 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.kt
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.kt
@@ -4,14 +4,9 @@ fun main() {
     val n = readLine()!!.toInt()
     val originalX = readLine()!!.split(" ").map { it.toInt() }
     val sortedX = originalX.distinct().sorted()
-    val hashX = HashMap<Int, Int>()
-    var index = 0
-    sortedX.forEach {
-        hashX[it] = index++
-    }
     val answer = StringBuilder()
     originalX.forEach {
-        answer.append("${hashX[it]} ")
+        answer.append("${sortedX.binarySearch(it)} ")
     }
     println(answer)
 }
