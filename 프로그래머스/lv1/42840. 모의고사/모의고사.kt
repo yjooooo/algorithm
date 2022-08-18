b93db344a05fcfb825221df6c821e0e7f8446e3d
Yunjoo Son <68374234+yjooooo@users.noreply.github.com>
2022-08-19 01:14:30 +0900

    [level 1] Title: 모의고사, Time: 4.44 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -0,0 +1,29 @@
+class Solution {
+    private val a = listOf(1, 2, 3, 4, 5)
+    private val b = listOf(2, 1, 2, 3, 2, 4, 2, 5)
+    private val c = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+    fun solution(answers: IntArray): IntArray {
+        var answer = mutableListOf<Int>()
+        val scores = intArrayOf(0, 0, 0)
+
+        for ((i, ans) in answers.withIndex()) {
+            if (ans == a[i % a.size]) scores[0]++
+            if (ans == b[i % b.size]) scores[1]++
+            if (ans == c[i % c.size]) scores[2]++
+        }
+
+        var max = scores[0]
+        answer.add(1)
+        for (i in 1 until scores.size) {
+            if (max <= scores[i]) {
+                if (max < scores[i]) {
+                    max = scores[i]
+                    answer.clear()
+                }
+                answer.add(i + 1)
+            }
+        }
+
+        return answer.toIntArray()
+    }
+}
