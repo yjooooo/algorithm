c9b9b2f495212afcba32f84569ae453fa0baa210
Yunjoo Son <68374234+yjooooo@users.noreply.github.com>
2022-10-16 19:20:16 +0900

    [level 2] Title: 카펫, Time: 12.53 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.kt
+++ 프로그래머스/lv2/42842. 카펫/카펫.kt
@@ -1,10 +1,12 @@
+import kotlin.math.sqrt
+
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
         var answer = intArrayOf()
-        for (yellowH in 1..(yellow / 2) + 1) {
+        for (yellowH in 1..sqrt(yellow.toFloat()).toInt()) {
             if(yellow % yellowH == 0){
                 val yellowW = yellow / yellowH
-                if (yellowH <= yellowW && ((yellowW + 2) * (yellowH + 2) == brown + yellow)) {
+                if ((yellowW + 2) * (yellowH + 2) == brown + yellow) {
                     answer += yellowW + 2
                     answer += yellowH + 2
                 }
